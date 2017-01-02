f767a3b13b1383fe807ef6518ca6fa5b492ae104
Daniel <budyniowski@gmail.com>
2017-01-02 15:33:43 +0100

    user persistance from admin panel

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,10 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Create</h1>
-</body>
-</html>
+@extends('layouts.admin')
+@section('content')
+    <h1>Create User</h1>
+    {!! Form::open(['method' => 'POST', 'action' => 'AdminUsersController@store', 'files'=>true]) !!}
+    
+
+        <div class='form-group'>
+        {!! Form::label('file', 'File: ') !!}
+        {!! Form::file('file', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class='form-group'>
+            {!! Form::label('name', 'User Name: ') !!}
+            {!! Form::text('name', null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class='form-group'>
+            {!! Form::label('email', 'Email ') !!}
+            {!! Form::email('email', null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class='form-group'>
+            {!! Form::label('role_id', 'Role: ') !!}
+            {!! Form::select('role_id', ['' => 'Chose option'] + $roles, null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class='form-group'>
+            {!! Form::label('password', 'Password: ') !!}
+            {!! Form::password('password', ['class' => 'form-control']) !!}
+        </div>
+
+        <div class='form-group'>
+            {!! Form::label('is_ative', 'Status: ') !!}
+            {!! Form::select('is_active', array(1 => 'Active', 0 => 'Not Active') , 0, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class='form-group'>
+            {!! Form::submit('Create User', ['class' => 'btn btn-info']) !!}
+        </div>
+        
+    
+    {!! Form::close() !!}
+
+    @include('includes.form_errors')
+
+
+@endsection
